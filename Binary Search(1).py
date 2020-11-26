d5cc2bcfe05e82339826a33104c51e23dda1fd4d
Junga-a <>
2020-11-26 19:56:59 +0900

    third commit

--- Binary Search(1).py
+++ Binary Search(1).py
@@ -1,5 +1,5 @@
 #이진 탐색은 찾으려는 데이터와 중간점 위치에 있는 데이터를 반복적으로 비교해서 원하는 데이터를 찾는 과정이다.
-#시간 복잡도는 O(logN)으로 매우 작다.
+#시간 복잡도는 O(logN)으로 매우 작다..
 def binary_search(array, target, start, end):
     if start>end:
         return None
